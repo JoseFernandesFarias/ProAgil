5cbf21f6ffa67093a1d5fd1f3bd9c725c204d1de
JoseFernandesFarias <josefernandes34sp@gmail.com>
2021-03-22 18:48:34 -0300

    atualizacao

---
+++ Proagil.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Proagil.WebApi.Data;
+using Proagil.WebApi.Model;
+
+namespace Proagil.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public DataContext _context { get; }
+        public WeatherForecastController(DataContext context)
+        {
+            this._context = context;
+
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<Evento>> Get()
+        {
+            try
+            {   var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
+
+            
+        }
+
+        //[Route("[controller]")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> Get(int id)
+        {
+
+               try
+            {   var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
+
+
+        }
+    }
+}
