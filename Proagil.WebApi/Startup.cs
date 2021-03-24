2979fec3f3a6f7ecda9dda8164e5c2f20898a17c
JoseFernandesFarias <josefernandes34sp@gmail.com>
2021-03-23 23:52:37 -0300

    include new features of Angular

--- Proagil.WebApi/Startup.cs
+++ Proagil.WebApi/Startup.cs
@@ -29,6 +29,7 @@ namespace Proagil.WebApi
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +39,8 @@ namespace Proagil.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
